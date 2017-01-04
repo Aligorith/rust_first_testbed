ee96ea9ea773b3a3780fad576c3f7b195a53416e
Joshua Leung <aligorith@gmail.com>
2017-01-05 02:52:36 +1300

    Refactor: Split off the command handling into separate functions

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,57 @@ struct DemoProgramEntry {
 }
 
 
+// Helper function: Print demo-command table
+fn print_demo_listing(table: &Vec<DemoProgramEntry>) {
+	println!("Choose which demo to run:");
+		for (i, item) in table.iter().enumerate() {
+			println!(" {index}) {description}",
+		         index = i + 1,
+		         description = item.name);
+	}
+	println!(" ---");
+	println!(" z/x/exit - To exit");
+}
+
+// Helper function: Handle command input (single round)
+fn handle_command_input(table: &Vec<DemoProgramEntry>) {
+	// Show prompt
+	terminal_utils::show_prompt("\n> ");
+	
+	// Read a line of text into the buffer
+	let mut raw_input = String::new();
+	io::stdin().read_line(&mut raw_input).expect("Please enter one of the numbers above...");
+	
+	// Strip off extra whitespace (including the newline)
+	let command = raw_input.trim();
+	
+	// Try to treat the input as a number (common case)
+	match command.parse::<usize>() {
+		Ok(value) => {
+			// We got a number, so treat it as a demo program index
+			let index = value - 1;
+			if (0 <= index) && (index < table.len()) {
+				(table[index as usize].cb)();
+			}
+			else {
+				println!("Unknown command number!");
+			}
+		},
+		Err(e) => {
+			// We got a string, so maybe it was the exit button?
+			match command {
+				// These two should be the same here - they both exit
+				//"e" => break,
+				"z" | "x" | "exit" => process::exit(0),
+				
+				// Catch all unknown commands
+				_   => println!("Unknown command!")
+			}
+		}
+	}	
+}
+
+
 // Main entrypoint
 fn main() {
 	let table = vec![
@@ -85,47 +136,11 @@ fn main() {
 	
 	loop {
 		// Print listing of available commands
-		println!("Choose which demo to run:");
-		for (i, item) in table.iter().enumerate() {
-			println!(" {index}) {description}",
-			         index = i + 1,
-			         description = item.name);
-		}
-		println!(" ---");
-		println!(" z/x/exit - To exit");
+		print_demo_listing(&table);
 		
 		
 		// Get command input, and react to it...
-		terminal_utils::show_prompt("\n> ");
-		
-		let mut raw_input = String::new();
-		io::stdin().read_line(&mut raw_input).expect("Please enter one of the numbers above...");
-		
-		let command = raw_input.trim();
-		
-		match command.parse::<usize>() {
-			Ok(value) => {
-				// We got a number, so treat it as a demo program index
-				let index = value - 1;
-				if (0 <= index) && (index < table.len()) {
-					(table[index as usize].cb)();
-				}
-				else {
-					println!("Unknown command number!");
-				}
-			},
-			Err(e) => {
-				// We got a string, so maybe it was the exit button?
-				match command {
-					// These two should be the same here - they both exit
-					"e" => break,
-					"z" | "x" | "exit" => process::exit(0),
-					
-					// Catch all unknown commands
-					_   => println!("Unknown command!")
-				}
-			}
-		}
+		handle_command_input(&table);
 		
 		// If still running, add some blank lines to seaprate the output
 		println!("\n\n");
