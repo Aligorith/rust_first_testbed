7daacc7dcb389fa17d18112caed8571bddab8cd7
Joshua Leung <aligorith@gmail.com>
2017-01-04 16:53:31 +1300

    First test - A simple loop, with some simple math (scaled total)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-	println!("Hello World");
+	let fac = 0.5;      // immutable constant
+	let mut total = 0.0; // running total sum - can be mutable
+	
+	// Sum things up
+	for i in 0..10 {
+		let x = i as f64; // XXX: This seems clunky, but we can't multiply a float by an int, hence the cast here
+		let val = (fac * x) + total;
+		
+		println!("Combining {0} and {1} => {2}", i, total, val);
+		// total += x;
+		total = val;
+	}
+	
+	println!("Total Sum is {}", total);
 }
